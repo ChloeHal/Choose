9c7788e0f44af3fd03e5dddb511f8d1aea2e12d3
ChloeHal <119426862+ChloeHal@users.noreply.github.com>
2023-03-22 00:25:22 +0100

    1 binaries uploaded

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -3,10 +3,10 @@ import './question.css';
 
 function Question({ question, onAnswer }) {
   return (
+    
     <div className="question-card">
-              <img src="./public/book/choose.png" />
-
-      <h1>I'll reveal your desired book upon your answer.</h1>
+       <img src="./public/book/choose.png" />
+<h1>I'll reveal your desired book upon your answer.</h1>
       <p>{question}</p>
       <div className="wrap">
         <button onClick={() => onAnswer('yes')}>Yes</button>
