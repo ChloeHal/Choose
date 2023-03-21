5d36b4b312399b2ed0608eb4e83283c0929521b2
ChloeHal <119426862+ChloeHal@users.noreply.github.com>
2023-03-21 17:07:26 +0100

    1 file modified

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -4,12 +4,18 @@ function Result({ answers, onRestart }) {
   return (
     <div>
       <p>Thank you for answering the questions!</p>
-      <p>Here are your answers:</p>
+      <p>Here is the book your answers:</p>
       <ul>
         {answers.map((answer, index) => (
           <li key={index}>{answer}</li>
         ))}
       </ul>
+      <div className="card">
+        <img src="./public/book/test.jpg" />
+        <p className="title">Titre</p>
+        <p className="author">Auteur</p>
+        <p classname="themes">Thèmes</p>
+      </div>
       <button onClick={() => onRestart()}>Restart Quiz</button>
     </div>
   );
