ac51479b409c7ab86d0ec6f91ff20614b137d58f
ChloeHal <119426862+ChloeHal@users.noreply.github.com>
2023-03-22 00:22:34 +0100

    1 binaries uploaded

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -4,6 +4,8 @@ import './question.css';
 function Question({ question, onAnswer }) {
   return (
     <div className="question-card">
+              <img src="./public/book/choose.png" />
+
       <h1>I'll reveal your desired book upon your answer.</h1>
       <p>{question}</p>
       <div className="wrap">
