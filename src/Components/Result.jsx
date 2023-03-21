0341106d16066c3eaf0643101587163129780bd1
ChloeHal <119426862+ChloeHal@users.noreply.github.com>
2023-03-21 17:09:13 +0100

    1 file modified

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 function Result({ answers, onRestart }) {
   return (
     <div>
-      <p>Thank you for answering the questions!</p>
-      <p>Here is the book your answers:</p>
+      <p>Following your answers, here is the book you have to read:</p>
       <ul>
         {answers.map((answer, index) => (
           <li key={index}>{answer}</li>
