e4440cbbcf9f0bcd3cfe5afd1b220fab2897237e
ChloeHal <119426862+ChloeHal@users.noreply.github.com>
2023-03-21 17:58:50 +0100

    3 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Question from './Components/Question';
 import Result from './Components/Result';
+import axios from 'axios';
 
 const questions = {
   fiction: [
